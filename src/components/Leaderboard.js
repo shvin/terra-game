8dd545cf6c5397ffb27d528d21e4089358156a6d
shvin <ashvincodes@gmail.com>
2022-04-14 21:11:05 -0400

    leaderboard init

---
+++ src/components/Leaderboard.js
@@ -0,0 +1,17 @@
+import '../App.css'
+import React from 'react'
+
+export const Leaderboard = () => {
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="leaderboard">
+                    <h2>Leaderboard</h2>
+                    <div className="leaderboard-list">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
